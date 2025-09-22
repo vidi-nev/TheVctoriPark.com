60442b44dca61c1e022301db9be12d42bbf6cf7d
vidi-nev <danilvidinev283@gmail.com>
2025-09-22 18:22:50 +0300

    Delete park directory

--- park/scrip.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('themeToggle').addEventListener('click', function() {
-    const currentTheme = document.body.className;
-    if (currentTheme === 'light-theme') {
-        document.body.className = 'dark-theme';
-    } else {
-        document.body.className = 'light-theme';
-    }
-});
